bcda97f551422190210f5d37f4db3d18db2ab49d
NightHao <48900334+NightHao@users.noreply.github.com>
2022-10-19 22:17:30 +0800

    Add files via upload

---
+++ BTS_IU/BTS_G.xcodeproj/Assets.xcassets/BTS_GApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTS_GApp.swift
+//  BTS_G
+//
+//  Created by nighthao on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct BTS_GApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
